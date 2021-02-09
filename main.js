d0eb16d12a80b52a052c2d387de9cd02cdc2c81e
Nayan Jain <71127472+NayanJain7@users.noreply.github.com>
2021-02-09 22:46:51 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log("User signed out.");
+    });
+  }
+
+  function onSignIn(googleUser) {
+    document.getElementById("logreg-forms").style.display = "none";
+
+    
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+   
+    document.getElementById("image").src=profile.getImageUrl();
+    document.getElementById("name").value=profile.getName();
+    document.getElementById("email").value=profile.getEmail();
+
+    // console.log('Full Name: ' + profile.getName());
+
+
+  
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+
+  }
